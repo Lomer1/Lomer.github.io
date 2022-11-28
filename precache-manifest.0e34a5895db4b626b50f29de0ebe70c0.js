45457e12a038fb6f9821410f7b461ce67cf46b4b
Mihail <94056256+Lomer1@users.noreply.github.com>
2022-11-28 21:16:37 +0300

    Add files via upload

---
+++ precache-manifest.0e34a5895db4b626b50f29de0ebe70c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64d8521e2852ac568b7e",
+    "url": "/static/css/main.c71070bc.chunk.css"
+  },
+  {
+    "revision": "64d8521e2852ac568b7e",
+    "url": "/static/js/main.c7d17316.chunk.js"
+  },
+  {
+    "revision": "4db315ee7cccd8473f08",
+    "url": "/static/js/runtime~main.d56670ee.js"
+  },
+  {
+    "revision": "bff41fe8fcbff4b941ec",
+    "url": "/static/js/2.478ec23f.chunk.js"
+  },
+  {
+    "revision": "6515e591187948cb8a6a",
+    "url": "/static/js/3.f79af52e.chunk.js"
+  },
+  {
+    "revision": "094fa928ad4c56addeed6173062606c0",
+    "url": "/index.html"
+  }
+];
